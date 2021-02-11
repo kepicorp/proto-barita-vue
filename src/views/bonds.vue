c0e6ae561269a0fc8c81c224a2a4319d3fe3be4b
Kepi Corp <webmaster@kepi.site>
2021-02-11 12:40:41 -0500

    update with more data and refresh button from FFDC

--- src/views/bonds.vue
+++ src/views/bonds.vue
@@ -1,7 +1,14 @@
 <template>
     <div class="row">
-        <div class="col s12 m12">
+        <div class="col s2 m2">
             <h4>Bonds</h4>
+        </div>
+        <div class="col s10 m10">
+            <button v-if="!newdata" v-on:click="updateSource()" class="btn waves-effect waves-light">Load from FFDC
+            </button>
+            <button v-if="newdata" class="btn waves-effect waves-light" disabled>Loaded</button>
+        </div>
+        <div class="col s12 m12">
             <div class="card">
               <table class="bordered highlight">
                 <thead>
@@ -14,8 +21,8 @@
                   </tr>
                 </thead>
                 <tbody>
-                  <tr v-for="item in trades.bonds"
-                    v-bind:key="item.dealNumber">
+                  <tr v-for="item in data.trades.bonds"
+                    v-bind:key="item">
                     <td class="left-align">{{ item.securityId }}</td>
                     <td class="left-align">{{ item.portfolio }}</td>
                     <td class="left-align" v-bind:class="{'red lighten-2': item.purchaseSell === 'P', 'green lighten-2': item.purchaseSell === 'S'}">{{ item.purchaseSell }}</td>
@@ -34,13 +41,35 @@
 
 <script>
 import { data } from '../kondor.js'
+import { data2 } from '../opics.js'
+
 export default {
   name: 'Bonds',
   data () {
-    return data;
+    return {
+      newdata: false,
+    }
+  },
+  computed: {
+    data() {
+      if (!this.newdata) {
+        return data;
+      } else {
+        return data2;
+      }
+    }
+  },
+  methods: {
+    async updateSource() {
+      setTimeout( () => {
+          //this.$router.push('/bonds');
+          this.newdata = true;
+        }, 1500);  
+    }
   }
 }
 </script>
 
 <style scoped>
+
 </style>
